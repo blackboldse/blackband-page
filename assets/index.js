1b1fdde3ec944b2abbc7053ec4260a835bb551fc
BlackBold <toantran014@gmail.com>
2022-06-10 22:42:10 +0700

    Fixed class button done

--- assets/index.js
+++ assets/index.js
@@ -1,8 +1,18 @@
-var counter = 1;
-setInterval(function() {
-  document.getElementById('slide' + counter).className = true;
-  counter++;
-  if(counter > 4){
-    counter = 1;
+const btnBuyTickets = document.querySelectorAll('.js-buy-tickets')
+const modal = document.querySelector('.js-modal')
+const modalClose = document.querySelector('.js-modal-close')
+
+
+  function showBuyTickets(){
+    modal.classList.add('open');
   }
-}, 5000);
+
+  function hideBuyTickets() {
+    modal.classList.remove('open')
+  }
+
+  for(const btnBuyTicket of btnBuyTickets) {
+    btnBuyTicket.addEventListener('click', showBuyTickets)
+  }
+
+  modal.addEventListener('click', hideBuyTickets)
