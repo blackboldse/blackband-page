be45131e435d6c39212f180392595d4f5472048e
BlackBold <toantran014@gmail.com>
2022-06-10 23:14:00 +0700

    Add stopPropagation for modal buy tickets

--- assets/index.js
+++ assets/index.js
@@ -1,18 +1,24 @@
 const btnBuyTickets = document.querySelectorAll('.js-buy-tickets')
 const modal = document.querySelector('.js-modal')
 const modalClose = document.querySelector('.js-modal-close')
+const modalContainer = document.querySelector('.js-modal-container')
 
 
   function showBuyTickets(){
     modal.classList.add('open');
+  }
+  for(const btnBuyTicket of btnBuyTickets) {
+    btnBuyTicket.addEventListener('click', showBuyTickets)
   }
 
   function hideBuyTickets() {
     modal.classList.remove('open')
   }
 
-  for(const btnBuyTicket of btnBuyTickets) {
-    btnBuyTicket.addEventListener('click', showBuyTickets)
-  }
+  modalClose.addEventListener('click', hideBuyTickets)
 
   modal.addEventListener('click', hideBuyTickets)
+
+  modalContainer.addEventListener('click', function(event) {
+    event.stopPropagation()
+  })
